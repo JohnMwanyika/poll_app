2df5fba4448a6eb17ea4133d42134c1665c51305
John Mwanyika <87723962+JohnMwanyika@users.noreply.github.com>
2023-03-07 17:17:30 +0300

    display individual questions

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse, Http404
 
 from .models import Question
@@ -7,16 +7,17 @@ from .models import Question
 # Create your views here.
 def index(req):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = '<br>'.join([q.question_text for q in latest_question_list])
-
-    return render(req, 'polls/index.html',{'latest_question_list':latest_question_list})
+    # output = '<br>'.join([q.question_text for q in latest_question_list])
+    context = {'latest_question_list':latest_question_list}
+    return render(req, 'polls/index.html',context)
 
 
 def detail(req, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(req, 'polls/detail.html',{'question':question})
 
 
